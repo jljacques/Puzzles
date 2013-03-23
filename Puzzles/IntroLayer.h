5d87dc493b1f24bc98fa6a9cfcd54af29600ed9c
Jeremy Jacques <jacquesjeremy@Jeremy-Jacques.local>
2013-03-23 19:56:34 -0400

    Initial Commit

---
+++ Puzzles/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Puzzles
+//
+//  Created by Jeremy Jacques on 13-03-23.
+//  Copyright Jeremy Jacques 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
